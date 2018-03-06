c55586cae5fb4fe08f75d63c86089e7aebc362ca
AmyYang96 <hpfanamy@gmail.com>
2018-03-05 21:32:19 -0800

    Log in and create account routed

---
+++ inspect/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App, {CreateAccount} from './App';
+import registerServiceWorker from './registerServiceWorker';
+import {Login} from './Login';
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
